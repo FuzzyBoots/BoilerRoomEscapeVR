9e92b3d6ff7781524deb9507e94f6376f00b3091
Sean Duggan <Sean.Duggan@gmail.com>
2023-10-22 00:21:43 -0400

    Fixed ending and hints

--- Assets/Scripts/EndScript.cs
+++ Assets/Scripts/EndScript.cs
@@ -1,23 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class EndScript : MonoBehaviour
 {
     [SerializeField] private TMP_Text _endingText;
     // Start is called before the first frame update
+    [SerializeField] private XROrigin _XROrigin;
+
     void Start()
     {
         if (_endingText == null)
         {
             Debug.LogError("No ending text assigned!");
         }
+
+        if (_XROrigin == null)
+        {
+            _XROrigin = FindObjectOfType<XROrigin>();
+            if (_XROrigin == null)
+            {
+                Debug.LogError("No valid XR Origin was found in the scene.");
+            }
+        }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        // Stop the player?
-        _endingText.gameObject.SetActive(true);
+        if (other.CompareTag("Player"))
+        {
+            _endingText.gameObject.SetActive(true);
+
+            ContinuousMoveProviderBase moveBase = _XROrigin.GetComponent<ContinuousMoveProviderBase>();
+            if (moveBase != null)
+            {
+                moveBase.enabled = false;
+            }
+        }
+        
     }
 }
